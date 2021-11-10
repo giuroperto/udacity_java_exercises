3c8a1ad0b956986a1097048daa5bc178048a7fb6
Giu Roperto <giu.roperto@gmail.com>
2021-11-10 18:38:53 -0300

    refactoring contact manager to get user input instead of hardcoded

--- ContactManager/src/Main.java
+++ ContactManager/src/Main.java
@@ -1,31 +1,83 @@
 import java.util.Scanner;
 
 public class Main {
+    static ContactsManager myContactsManager = new ContactsManager();
+    static Scanner scanner = new Scanner(System.in);
+    static boolean showMenu = true;
+
     public static void main(String[] args) {
-        ContactsManager myContactsManager = new ContactsManager();
 
-        Scanner scanner = new Scanner(System.in);
+        while (showMenu) {
+            showMenu();
+        }
+        scanner.close();
+    }
 
-        System.out.println("Write your friend's name");
+    private static void showMenu() {
+        System.out.println("-------------------------------------------");
+        System.out.println("-----------------CONTACTS------------------");
+        System.out.println("-------------------------------------------");
+        System.out.println("Please choose one of the following:");
+        System.out.println("Press 1 + ENTER to add a new contact");
+        System.out.println("Press 2 + ENTER to search a contact");
+        System.out.println("Press 3 + ENTER to exit");
+
+        int option = scanner.nextInt();
+        scanner.nextLine();
+
+        if (option == 1) {
+            addNewContact();
+        } else if (option == 2) {
+            searchContact();
+        } else if (option == 3) {
+            exitProgram();
+        } else {
+            System.out.println("Invalid option");
+        }
+    }
+
+    private static void exitProgram() {
+        System.out.println("Exiting...");
+        showMenu = false;
+    }
+
+    private static void addNewContact() {
+        System.out.println("What's your contact's name?");
         String name = scanner.nextLine();
-        System.out.println(name);
 
-        Contact myFriend1 = new Contact(name, "123456789");
-        myContactsManager.addContact(myFriend1);
+        System.out.println("And what's your contact's phone?");
+        String phone = scanner.nextLine();
 
-        Contact myFriend2 = new Contact("Nelly", "123456788");
-        myContactsManager.addContact(myFriend2);
+        Contact newContact = new Contact(name, phone);
+        myContactsManager.addContact(newContact);
 
-        Contact myFriend3 = new Contact("Sally", "123456787");
-        myContactsManager.addContact(myFriend3);
+        System.out.println("Added " + name + "'s contact! Phone: " + phone);
+        goBackToMenuOrExit();
+    }
 
-        Contact myFriend4 = new Contact("Shelly", "123456786");
-        myContactsManager.addContact(myFriend4);
-
-        Contact myFriend5 = new Contact("Wally", "123456785");
-        myContactsManager.addContact(myFriend5);
-
+    private static void searchContact() {
+        System.out.println("What name you'd like to search for?");
+        String name = scanner.nextLine();
         Contact searchedContact = myContactsManager.searchContact(name);
-        System.out.println(searchedContact.phoneNumber);
+        System.out.println("====================================");
+        System.out.println(name + "'s phone is: " + searchedContact.phoneNumber);
+        System.out.println("====================================");
+        goBackToMenuOrExit();
+    }
+
+    private static void goBackToMenuOrExit() {
+        System.out.println("To exit, press 0");
+        System.out.println("To continue adding or searching, press 1");
+
+        int response = scanner.nextInt();
+        scanner.nextLine();
+
+        if (response == 0) {
+            exitProgram();
+        } else if (response == 1) {
+            showMenu();
+        } else {
+            System.out.println("Invalid option!");
+        }
     }
 }
