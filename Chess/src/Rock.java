9fb83efaf336d9581e9ec9570c13a5f80ffd1646
Giu Roperto <giu.roperto@gmail.com>
2021-11-30 00:04:22 -0300

    updating chess logic

--- Chess/src/Rock.java
+++ Chess/src/Rock.java
@@ -2,6 +2,12 @@ public class Rock extends Piece {
 
     @Override
     boolean isValidMove(Position newPosition) {
+        // First call the parent's method to check for the board bounds
+        if (!super.isValidMove(position)) {
+            return false;
+        }
+
+        // If we passed the first test then check for the specific rock movement
         if (newPosition.column == position.column || newPosition.row == position.row) {
             return true;
         } else {
@@ -9,20 +15,3 @@ public class Rock extends Piece {
         }
     }
 }
-
-
-//class Rock extends Piece{
-//    boolean isValidMove(Position newPosition){
-//        // First call the parent's method to check for the board bounds
-//        if(!super.isValidMove(position)){
-//            return false;
-//        }
-//        // If we passed the first test then check for the specific rock movement
-//        if(newPosition.column == this.column && newPosition.row == this.row){
-//            return true;
-//        }
-//        else{
-//            return false;
-//        }
-//    }
-//}
